8a71ce566ff1a1c6b260bbbab7a5e3630e97d412
Mark Muskardin <markmuskardin@gmail.com>
2011-07-04 13:03:49 -0700

    added jquery ajax call boilerplate code

--- js/application.js
+++ js/application.js
@@ -1,3 +1,42 @@
 $(function() {
-  alert('yes');
+   
+  Synesthesia = {
+    init:function(){
+      Synesthesia.createNeuron();
+      Synesthesia.deleteMeme();
+    },
+    createNeuron:function(){
+      $('#createNeuron').click(function(){
+         $.ajax({
+           type: 'post',
+           data: { '_method': 'delete', 'authenticity_token' : "rails_authenticity_token" },
+           url: this.href,
+           success: function(){
+
+           },
+           error: function(){
+
+           }
+         });
+       });
+    },
+    deleteMeme:function(){
+      $('#deleteMeme').click(function(){
+        $.ajax({
+          type: 'post',
+          data: { '_method': 'delete', 'authenticity_token' : "rails_authenticity_token" },
+          url: this.href,
+          success: function(){
+
+          },
+          error: function(){
+    
+          }
+        });
+      });
+    }
+  }
+  
+  Synesthesia.init();
+  
 });

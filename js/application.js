920cc588e89aa0f84e13565b4cc9759a10fd77e6
Mark Muskardin <markmuskardin@gmail.com>
2011-07-04 12:54:05 -0700

    renamed js function

--- js/application.js
+++ js/application.js
@@ -1,11 +1,3 @@
-// $(function() {
-//   tinyMCE.init({
-//     mode : "textareas",
-//     theme: "advanced",
-//     theme_advanced_toolbar_location : "top",
-//     theme_advanced_buttons1 : "bold,italic,underline,strikethrough,justifycenter,justifyright,justifyfull,separator",
-//     theme_advanced_buttons2: "",
-//     theme_advanced_buttons3: "",
-//     theme_advanced_buttons4: "",
-//   })
-// });
+$(function() {
+  alert('yes');
+});

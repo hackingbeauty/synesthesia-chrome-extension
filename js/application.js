8b68b889c8ef7f523e205937b8976721dbd48ecf
Mark Muskardin <markmuskardin@gmail.com>
2011-07-04 12:43:56 -0700

    Added tinymce code, but seems to be conflicting with chrome js code

--- js/application.js
+++ js/application.js
@@ -1,89 +1,11 @@
-// function pasteSelection() {
-//   chrome.tabs.getSelected(null, function(tab) {
-//     chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function (response) {
-//       var text = document.getElementById('text'); 
-//       text.innerHTML = response.data;
-//     });
-//   });
-// }
-
 // $(function() {
-//   Synesthesia =  {
-//     init:function(){
-//       chrome.tabs.getSelected(null, function(tab) {
-//         chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function (response) {
-//           var text = document.getElementById('text'); 
-//           text.innerHTML = response.data;
-//         });
-//       });
-//     }
-//   }
-//   
-//   Synesthesia.init();
-//   
+//   tinyMCE.init({
+//     mode : "textareas",
+//     theme: "advanced",
+//     theme_advanced_toolbar_location : "top",
+//     theme_advanced_buttons1 : "bold,italic,underline,strikethrough,justifycenter,justifyright,justifyfull,separator",
+//     theme_advanced_buttons2: "",
+//     theme_advanced_buttons3: "",
+//     theme_advanced_buttons4: "",
+//   })
 // });
-// $(function() {
-//   
-//   Synesthesia = {
-//     selectedText:null,
-//     init:function(){
-//       Synesthesia.getSelectedText();
-//     },
-//     getSelectedText:function(){
-//       $('#clipAction').click(function(){
-//         // chrome.tabs.getSelected(null, function(tab) {
-//         //    chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function (response) {
-//         //      var text = document.getElementById('text'); 
-//         //      text.innerHTML = response.data;
-//         //      alert(text.innerHTML);
-//         //    });
-//         //  });
-//         //  Synesthesia.selectedText = window.getSelection().toString();
-//         // Synesthesia.createNeuron();
-//       });
-//     },
-//     createNeuron:function(){
-//       $.blockUI({ message: $('<div>' + Synesthesia.selectedText + '</div>' +
-//       '<button id="save">Save</button> ' +
-//       '<button id="cancel">Cancel</button>'), css: { width: '275px' } }); 
-//       $('.blockOverlay').attr('title','Click to unblock').click($.unblockUI); 
-//       $('#save').click(function(){
-//         $.blockUI({ message: "<h1>Remote call in progress...</h1>" });
-//         $.ajax({ 
-//           url: 'wait.php', 
-//           cache: false, 
-//           complete: function() { 
-//              $.unblockUI(); // unblock when remote call returns 
-//           } 
-//         });
-//       });
-//       $('#cancel').click(function(){
-//         $.unblockUI(); 
-//         return false; 
-//       });
-//       
-//       // $('#yes').click(function() { 
-//       //    // update the block message 
-//       //    $.blockUI({ message: "<h1>Remote call in progress...</h1>" }); 
-//       // 
-//       //    $.ajax({ 
-//       //        url: 'wait.php', 
-//       //        cache: false, 
-//       //        complete: function() { 
-//       //            // unblock when remote call returns 
-//       //            $.unblockUI(); 
-//       //        } 
-//       //    }); 
-//       // }); 
-//       // 
-//       // $('#no').click(function() { 
-//       //    $.unblockUI(); 
-//       //    return false; 
-//       // });
-//     }
-//   }
-//   
-//   Synesthesia.init();
-//   
-// });
-// 

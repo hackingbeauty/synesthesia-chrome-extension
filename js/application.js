2a16b6898c58f87a562b9872deb90c1696f9b0d3
Mark Muskardin <markmuskardin@gmail.com>
2011-07-09 12:49:00 -0700

    added login form and logic to query login status

--- js/application.js
+++ js/application.js
@@ -3,6 +3,24 @@ $(function() {
   window.Synesthesia = {
     init:function(){
       Synesthesia.cancel();
+      Synesthesia.loggedOut();
+    },
+    loggedOut:function(){
+      $.ajax({
+       type: 'get',
+       // url: 'http://synesthesia-note.herokuapp.com/evernote/is_logged_in.json',
+       url: 'http://localhost:3000/evernote/is_logged_in.json',
+       success: function(res){
+         if(!res.logged_in == true){
+           $('#login-form').removeClass("hide");
+         } else {
+           $('#main-form').removeClass("hide");
+         }
+       },
+       error: function(res){
+         $('#text').html('did not get the json response');
+       }
+      });
     },
     createNeuron:function(){ 
       var url   = window.currentUrl;
